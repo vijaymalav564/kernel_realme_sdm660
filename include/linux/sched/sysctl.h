10aee6e04f6c7e5aa0684cff541c28bd4987df41
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2018-08-09 17:19:54 -0700

    sched/core: fix userspace affining threads incorrectly
    Certain userspace applications, to achieve max performance, affines its
    threads to cpus that run the fastest. This is not always the
    correct strategy. For e.g. in certain architectures all the
    cores have the same max freq but few of them have a bigger
    cache. Affining to the cpus that have bigger cache is advantageous
    but such an application would end up affining them to all the cores.
    Similarly if an architecture has just one cpu that runs at max freq,
    it ends up crowding all its thread on that single core, which is
    detrimental for performance.
    
    To address this issue, we need to detect a suspicious looking affinity
    request from userspace and check if it links in a particular library.
    The latter can easily be detected by traversing executable vm areas
    that map a file and checking for that library name.
    When such a affinity request is found, change it to use a proper
    affinity. The suspicious affinity request, the proper affinity request
    and the library name can be configured by the userspace.
    
    Change-Id: I6bb8c310ca54c03261cc721f28dfd6023ab5591a
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -135,4 +135,9 @@ extern unsigned int sysctl_sched_boost;
 extern int sched_boost_handler(struct ctl_table *table, int write,
 			       void __user *buffer, size_t *lenp, loff_t *ppos);
 
+#define LIB_PATH_LENGTH 512
+extern char sched_lib_name[LIB_PATH_LENGTH];
+extern unsigned int sched_lib_mask_check;
+extern unsigned int sched_lib_mask_force;
+
 #endif /* _SCHED_SYSCTL_H */
