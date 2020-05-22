90cce962cafb3700012f2536159bfc771b7d1742
Anand Shekhawat <anandsingh215@yahoo.com>
2020-05-22 21:37:54 +0530

    drivers/net/wireless/ath/ath9k: fix -Wconstant-conversion warnings

--- drivers/net/wireless/ath/ath9k/eeprom.h
+++ drivers/net/wireless/ath/ath9k/eeprom.h
@@ -95,8 +95,8 @@
 #define AR9285_RDEXT_DEFAULT    0x1F
 
 #define ATH9K_POW_SM(_r, _s)	(((_r) & 0x3f) << (_s))
-#define FREQ2FBIN(x, y)		((y) ? ((x) - 2300) : (((x) - 4800) / 5))
-#define FBIN2FREQ(x, y)		((y) ? (2300 + x) : (4800 + 5 * x))
+#define FREQ2FBIN(int x, int y)		((y) ? ((x) - 2300) : (((x) - 4800) / 5))
+#define FBIN2FREQ(int x, int y)		((y) ? (2300 + x) : (4800 + 5 * x))
 #define ath9k_hw_use_flash(_ah)	(!(_ah->ah_flags & AH_USE_EEPROM))
 
 #define AR5416_VER_MASK (eep->baseEepHeader.version & AR5416_EEP_VER_MINOR_MASK)
