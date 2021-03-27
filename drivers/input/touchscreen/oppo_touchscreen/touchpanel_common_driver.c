9944398c78e8e1afe64c6403ca9709326e1e31e0
Salllz <sal235222727@gmail.com>
2021-03-27 03:42:30 +0000

    drivers: oppo_touchscreen: Mark IRQ as performance critical
    Signed-off-by: Salllz <sal235222727@gmail.com>

--- drivers/input/touchscreen/oppo_touchscreen/touchpanel_common_driver.c
+++ drivers/input/touchscreen/oppo_touchscreen/touchpanel_common_driver.c
@@ -4472,7 +4472,7 @@ int tp_register_irq_func(struct touchpanel_data *ts)
         }
         ret = request_threaded_irq(ts->irq, NULL,
                 tp_irq_thread_fn,
-                ts->irq_flags | IRQF_ONESHOT,
+                ts->irq_flags | IRQF_ONESHOT | IRQF_PERF_CRITICAL,
                 TPD_DEVICE, ts);
         if (ret < 0) {
             TPD_INFO("%s request_threaded_irq ret is %d\n", __func__, ret);
