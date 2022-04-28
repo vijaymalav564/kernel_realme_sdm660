68dd1830e0affcdef1a1264f0df8e4373b5c14aa
vijaymalav564 <jaymalav10@gmail.com>
2022-04-28 17:01:23 +0000

    Revert "sched/sysctl: Further tweak the scheduler"
    This reverts commit 70781dd3ea277ad13821fe49982803b23011910a.

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -298,6 +298,13 @@ static struct ctl_table kern_table[] = {
 		.extra2		= &three,
 	},
 #endif
+	{
+		.procname	= "sched_child_runs_first",
+		.data		= &sysctl_sched_child_runs_first,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec,
+	},
 #if defined(CONFIG_PREEMPT_TRACER) || defined(CONFIG_IRQSOFF_TRACER)
 	{
 		.procname       = "preemptoff_tracing_threshold_ns",
@@ -376,6 +383,15 @@ static struct ctl_table kern_table[] = {
 		.maxlen		= sizeof(unsigned int),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
+	},
+	{
+		.procname	= "sched_wakeup_granularity_ns",
+		.data		= &sysctl_sched_wakeup_granularity,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= sched_proc_update_handler,
+		.extra1		= &min_wakeup_granularity_ns,
+		.extra2		= &max_wakeup_granularity_ns,
 	},
 #ifdef CONFIG_SMP
 	{
