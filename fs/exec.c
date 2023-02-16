12d81a17631540b2a82aae7aa839f2b0e16f4d70
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2023-02-16 13:42:08 +0700

    fs: Guard ksu changes with `CONFIG_KSU_NO_KPROBES`
    * Only applied for kernel with broken kprobes
    
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1527,8 +1527,11 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+#ifdef CONFIG_KSU_NO_KPROBES
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			       void *envp, int *flags);
+#endif // CONFIG_KSU_NO_KPROBES
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1544,7 +1547,9 @@ static int do_execveat_common(int fd, struct filename *filename,
 	int retval;
 	bool is_su;
 
+	#ifdef CONFIG_KSU_NO_KPROBES
 	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	#endif
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
